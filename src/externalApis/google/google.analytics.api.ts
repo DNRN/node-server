af7d204c163b90171f661bf3d48bcf727877c5df
Dennis Christensen <dnrnmail@gmail.com>
2018-04-10 11:55:03 +0200

    g sum

--- src/externalApis/google/google.analytics.api.ts
+++ src/externalApis/google/google.analytics.api.ts
@@ -5,7 +5,7 @@ export class GoogleAnalytisApi {
     public static getAccounts(token: string) {
         return request({
             "method":"GET", 
-            "uri": 'https://www.googleapis.com/analytics/v3/management/accounts',
+            "uri": 'https://www.googleapis.com/analytics/v3/management/accountSummaries',
             "json": true,
             "headers": {
                 "Authorization": "Bearer " + token,
